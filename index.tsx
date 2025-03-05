be2462181988c136cb42ea982407cd1fae73cba8
Shivani Reddy <shivanireddy8141@gmail.com>
2025-03-05 12:29:28 -0600

    Add files via upload

--- index.tsx
+++ index.tsx
@@ -1,11 +1,22 @@
-import ReactDOM from "react-dom/client"
-import "./index.css"
-import { App } from "./App"
-import { AppContextProvider } from "./components/AppContextProvider"
+import { useRef, useState } from "react"
+import { AppContext } from "../../utils/context"
+import { AppContextProviderComponent } from "./types"
 
-const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
-root.render(
-  <AppContextProvider>
-    <App />
-  </AppContextProvider>
-)
+export const AppContextProvider: AppContextProviderComponent = ({ children }) => {
+  const cache = useRef(new Map<string, string>())
+  const [error, setError] = useState<string>("")
+
+  return (
+    <AppContext.Provider value={{ setError, cache }}>
+      {error ? (
+        <div className="RampError">
+          <h1 className="RampTextHeading--l">Oops. Application broken</h1>
+          <div className="RampBreak--l" />
+          Error: {error}
+        </div>
+      ) : (
+        children
+      )}
+    </AppContext.Provider>
+  )
+}
