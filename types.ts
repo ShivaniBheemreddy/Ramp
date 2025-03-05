043b17b02adf5624f729c8d214c94557daeb53d1
Shivani Reddy <shivanireddy8141@gmail.com>
2025-03-05 12:30:11 -0600

    Add files via upload

--- types.ts
+++ types.ts
@@ -1,10 +1,20 @@
-import { FunctionComponent } from "react"
+export type InputSelectItem = { label: string; value: string }
 
-type InputCheckboxProps = {
-  id: string | number
-  checked?: boolean
-  onChange: (newValue: boolean) => void
-  disabled?: boolean
+export type InputSelectProps<TItem> = {
+  label: string
+  defaultValue?: TItem | null
+  onChange: (value: TItem | null) => void
+  items: TItem[]
+  parseItem: (item: TItem) => InputSelectItem
+  isLoading?: boolean
+  loadingLabel: string
 }
 
-export type InputCheckboxComponent = FunctionComponent<InputCheckboxProps>
+export type DropdownPosition = {
+  top: number
+  left: number
+}
+
+export type InputSelectOnChange<TItem> = (selectedItem: TItem | null) => void
+
+export type GetDropdownPositionFn = (target: EventTarget) => DropdownPosition
